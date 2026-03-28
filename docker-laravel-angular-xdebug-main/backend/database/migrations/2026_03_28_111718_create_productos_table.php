553e33bff77dd2ae1be2890c9b9e80c6e08ef0d4
AnandaSMM <anandamarshalmagallanes@gmail.com>
2026-03-28 18:17:47 +0100

    proyecto montado

---
+++ docker-laravel-angular-xdebug-main/backend/database/migrations/2026_03_28_111718_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('users')->onDelete('cascade');
+
+            $table->string('nombre', 150);
+            $table->text('descripcion')->nullable();
+
+            $table->decimal('precio_venta', 10, 2)->nullable();
+            $table->decimal('precio_alquiler_dia', 10, 2)->nullable();
+
+            $table->boolean('vendido')->default(false);
+            $table->boolean('disponible')->default(true);
+
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};

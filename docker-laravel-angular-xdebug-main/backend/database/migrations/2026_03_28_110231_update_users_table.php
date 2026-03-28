3d1523d61d5be66a0d6ee92e7e6efa3f6c74a704
AnandaSMM <anandamarshalmagallanes@gmail.com>
2026-03-28 18:17:47 +0100

    proyecto montado

---
+++ docker-laravel-angular-xdebug-main/backend/database/migrations/2026_03_28_110231_update_users_table.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'nombre');
+            $table->string('telefono', 20)->nullable()->after('email');
+            $table->string('foto', 250)->nullable()->after('telefono');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('nombre', 'name');
+            $table->dropColumn(['telefono', 'foto']);
+        });
+    }
+};
---
+++ docker-laravel-angular-xdebug-main/backend/database/migrations/2026_03_28_111806_create_alquileres_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alquileres', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('usuario_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('producto_id')->nullable()->constrained('productos')->nullOnDelete();
+
+            $table->date('fecha_inicio')->nullable();
+            $table->date('fecha_fin')->nullable();
+
+            $table->decimal('precio_total', 10, 2)->nullable();
+
+            $table->enum('estado', ['activo', 'finalizado', 'cancelado'])->default('activo');
+
+            $table->boolean('opcion_compra')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alquileres');
+    }
+};
